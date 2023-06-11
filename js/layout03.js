b330dae9576383125780a3879f3c792adc839cc5
LJH <95152341+LeeJinHyuk002@users.noreply.github.com>
2023-06-11 16:05:49 +0900

    Add files via upload

--- js/layout03.js
+++ js/layout03.js
@@ -8,4 +8,50 @@ $(function () {
         dots: true,
     });
 
+
+    $('.m_slide').slick({
+        arrows: false,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+
+
+    $('.arrows .left').on('click', function () {
+        $('.m_slide').slick('slickPrev');
+    });
+    $(' .arrows .right').on('click', function () {
+        $('.m_slide').slick('slickNext');
+    });
 })
